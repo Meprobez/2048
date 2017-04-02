16f2eb5bf6f2854cedf7008682c5fc099eebb2a5
Victor Yurievich <meprobez@gmail.com>
2017-04-02 17:11:14 +0300

    step-01: Building grid

--- app/main/app.controller.js
+++ app/main/app.controller.js
@@ -1,8 +1,28 @@
 angular.module('2048')
 .controller('appController', appController);
 
-appController.$inject('gridService', 'gameManager');
-function appController(gridService, gameManager)
+//appController.$inject = ['gridService', 'gameManager'];
+function appController()
 {
 	var self = this;
+	self.$onInit = onInit;
+
+	function onInit()
+	{
+		self.grid = [];
+		self.cell = {
+			x:0,
+			y:0,
+		number:null	
+		};
+		for(var i=0; i<4;i++)
+			for(var j=0; j<4; j++)
+			{
+				self.cell.y = i;
+				self.cell.x = j;
+				self.grid.push(self.cell);
+			};
+		console.log(self.grid);
+	};
+	
 }
