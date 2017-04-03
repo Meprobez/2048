7dee4c6c9b1827ad72a2e248f2442c5c298d844f
Victor Yurievich <meprobez@gmail.com>
2017-04-03 08:54:25 +0300

    step-02: random numbers

--- app/main/app.gridService.service.js
+++ app/main/app.gridService.service.js
@@ -1,8 +1,52 @@
-angular.module('grid', [])
-.service('gridService', function() {
-  this.grid   = []; //x4
-  this.tiles  = []; //x4
-  // Size of the board
-  this.size   = 4;
-  // ...
-});
+angular.module('2048')
+.factory('gridService', gridService)
+
+
+function gridService() 
+{
+  var self = this;
+
+  self.grid   = []; //x4
+  self.tiles  = []; //x4
+  
+  var service = {
+    createGrid:createGrid,
+    returnGrid:returnGrid
+  };   
+  return service;
+
+function createGrid()
+{
+  self.cell = {
+			x:0,
+			y:0,
+		    number:null	
+		};
+  for(var i=0; i<4;i++)
+	for(var j=0; j<4; j++)
+	{
+		self.cell.y = i;
+		self.cell.x = j;
+		let cell = angular.copy(self.cell)
+		self.grid.push(cell);
+	};
+}
+
+function cellGenerator(grid,movesCounter)
+{
+	if(movesCounter)
+	{
+
+	}
+	else
+	{
+		let num1 = Math.random()
+	}
+
+}
+function returnGrid()
+{
+	createGrid();
+    return self.grid;
+}
+}
