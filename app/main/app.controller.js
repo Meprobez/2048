5f9a2ce14da592347de13e5400e35458bfed31e0
Victor Yurievich <meprobez@gmail.com>
2017-04-03 14:28:55 +0300

    step-1: New game is ready

--- app/main/app.controller.js
+++ app/main/app.controller.js
@@ -1,16 +1,49 @@
 angular.module('2048')
 .controller('appController', appController);
 
-appController.$inject = ['gridService'];
-function appController(gridService)
+appController.$inject = ['$scope','gridService'];
+function appController($scope,gridService)
 {
 	var self = this;
 	self.$onInit = onInit;
+	self.grid = [];
+	self.fieldSize = 4;
+	self.template = { url: 'main/app.html' };
+	self.score = 0;
+	self.best = 0;
+	self.newGame = newGame;
+	self.createField = createField;
 
 	function onInit()
 	{
-		self.grid = gridService.returnGrid();
-		console.log(self.grid);
+		self.grid = gridService.returnNewGrid();
+		self.createField(self.grid);
+		
 	};
+	function createField(grid)
+	{
+		self.field = [];
+		for(var i=0;i<self.fieldSize;i++)
+		{
+			self.field[i] = [];
+			for(var j=0; j<self.fieldSize;j++)
+			{
+				let step = i*self.fieldSize;
+				console.log(step);
+				self.field[i].push(grid[j+step]);
+			}
+		}
+		console.log(self.field);
+	}
+
+	function newGame()
+	{
+		if(self.score>self.best)
+			self.best = self.score;
+		self.score = 0;
+		self.grid = gridService.returnNewGrid();
+		self.createField(self.grid);
+
+	}
 	
 }
--- app/main/app.gridService.service.js
+++ app/main/app.gridService.service.js
@@ -11,24 +11,28 @@ function gridService()
   
   var service = {
     createGrid:createGrid,
-    returnGrid:returnGrid
+    returnNewGrid:returnNewGrid
   };   
   return service;
 
 function createGrid()
 {
+  self.grid = [];
   self.cell = {
 			x:0,
 			y:0,
 		    number:null	
 		};
+  var id = 0;
   for(var i=0; i<4;i++)
 	for(var j=0; j<4; j++)
 	{
+		self.cell.id = id;
 		self.cell.y = i;
 		self.cell.x = j;
 		let cell = angular.copy(self.cell)
 		self.grid.push(cell);
+		id++;
 	};
 }
 
@@ -40,13 +44,28 @@ function cellGenerator(grid,movesCounter)
 	}
 	else
 	{
-		let num1 = Math.random()
+		let num1 = getRandomInt(0, 16);
+		let num2 = getRandomInt(0, 16);
+		while(num1===num2)
+		{
+			num2 = getRandomInt(0, 16);
+		};
+		grid[num1].number = 2;
+		grid[num2].number = 2;
+		console.log(grid);
 	}
 
 }
-function returnGrid()
+function returnNewGrid()
 {
 	createGrid();
+	cellGenerator(self.grid,0)
     return self.grid;
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 }
